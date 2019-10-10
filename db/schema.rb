59bd7df84f398a5e7bdabc99123905035738b621
Alan Mareines <alanmareines@gmail.com>
2019-10-10 18:44:51 -0300

    added restaurants and reviews - migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_211447) do
+ActiveRecord::Schema.define(version: 2019_10_10_213115) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
